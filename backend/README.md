aab685c39ad6aaf40ef28776be4f86599fc1a31e
renaskitamura <105678722+renaskitamura@users.noreply.github.com>
2025-04-13 17:21:40 -0400

    Created azure function api

--- backend/README.md
+++ backend/README.md
@@ -1,1 +1,2 @@
 # Your backend lives here
+Created a Azure Function locally and binded to Azure CosmosDB account that handles the count of the visits in the website
--- frontend/main.js
+++ frontend/main.js
@@ -2,14 +2,14 @@ window.addEventListener('DOMContentLoader', (event) =>{
     getVisitCount();
 })
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
     let count = 30;
     fetch(functionApi).then(response =>{
         return response.json()
     }).then(response =>{
-        console.log("Website caller function API");
+        console.log("Website caller function API.");
         count = response.count;
         document.getElementById("counter").innerText = count;
     }).catch(function(error){
